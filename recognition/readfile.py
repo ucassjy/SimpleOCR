06a562284ec2f5d227ac8bd5cc13cd8dc7304f2a
ucassjy <470323747@qq.com>
2018-05-14 15:13:28 +0200

    REFACTOR : Move files

--- recognition/readfile.py
+++ recognition/readfile.py
@@ -5,21 +5,29 @@ import numpy as np
 import cv2
 
 step = 0
+MAX = 0
+Threshold = 50
+MAX_LENGTH = 15 * Threshold
 ff = open("/home/h/a/hanlins/Desktop/OCR/target_label.txt",'w')
 
 for filename in os.listdir('image_1000/'): #'/home/h/a/hanlins/Desktop/OCR/image_1000/'):
 #for i in range(1):
 #    filename = "TB1oAXWLXXXXXXoXXXXunYpLFXX.jpg"
     filename_new = "image_1000/" + filename #"/home/h/a/hanlins/Desktop/OCR/image_1000/" + filename
+
     img = cv2.imread(filename_new, cv2.IMREAD_GRAYSCALE)
-#    ret, img = cv2.threshold(img,12,255,cv2.THRESH_BINARY)
+    if img is None:
+        continue
+
+    #img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 20)
+    ret,img = cv2.threshold(img, 127,255,cv2.THRESH_TOZERO)
     step += 1
-    if (step > 20):
+    print(step)
+    if (step > 1000):
+
         exit()
 
     #img = np.array(img)
-    if img is None:
-        continue
 
     filename_suf = filename.split(".jpg")[0]
 
@@ -40,15 +48,9 @@ for filename in os.listdir('image_1000/'): #'/home/h/a/hanlins/Desktop/OCR/image
             cnt = np.array([[position[0], position[1]],[position[2], position[3]],[position[4], position[5]],[position[6], position[7]]])
             rect = cv2.minAreaRect(cnt)
             angle = rect[2]
-            #box = np.int0(cv2.boxPoints(rect))
+
             X_centre = rect[0][0]
             Y_centre = rect[0][1]
-            #
-            # if (filename_suf=="TB1..FLLXXXXXbCXpXXunYpLFXX" and Num == 13):
-            #     cv2.drawContours(img,[box],-1,(0,255,0),3)
-            #     cv2.imshow("Image",img)
-            #     cv2.waitKey(0)
-            #     exit()
 
             if (angle == -90.0 or angle == 0.0):
                 X_start = min(X)
@@ -56,10 +58,25 @@ for filename in os.listdir('image_1000/'): #'/home/h/a/hanlins/Desktop/OCR/image
                 Y_start = min(Y)
                 Y_end = max(Y)
                 crop_img = img[Y_start:Y_end, X_start:X_end]
-                plt.imshow(crop_img, cmap="gray")
-                print(label, Num, '0')
+                if (crop_img.shape[1] < crop_img.shape[0]):
+                    crop_img = np.rot90(crop_img)
+                x_s = Threshold
+                if crop_img.shape[0]==0:
+                    continue
+                y_s = int((crop_img.shape[1]/crop_img.shape[0]) * Threshold)
+                crop_img = cv2.resize(crop_img,(y_s, x_s))
+                rest = MAX_LENGTH - crop_img.shape[1]
+                if rest < 0:
+                    rest = 0
+                stack = np.ones([Threshold, rest]) * 255
+                crop_img = np.hstack((crop_img, stack))
+            #    print(crop_img.shape)
+            #    plt.imshow(crop_img, cmap="gray")
+            #    print(label, Num, '0')
+
                 rewrite_name = "rewrite/" + filename_suf + "_" + str(Num) + ".jpg" #"/home/h/a/hanlins/Desktop/OCR/rewrite/" + filename_suf + "_" + str(Num) + ".jpg"
-                plt.savefig(rewrite_name)
+                cv2.imwrite(rewrite_name,crop_img)
+                ff.write(rewrite_name + ',' + label)
                 Num += 1
 
             else:
@@ -70,14 +87,25 @@ for filename in os.listdir('image_1000/'): #'/home/h/a/hanlins/Desktop/OCR/image
                 RECT = (rect[0][0] + X_MAX, rect[0][1] + Y_MAX)
                 M = cv2.getRotationMatrix2D(RECT, angle, 1)
                 img_new = cv2.warpAffine(img_new, M, (Y_MAX*3, X_MAX*3))
-                print(X_MAX, Y_MAX)
-            #    plt.imshow(img_new)
-            #    plt.show()
-            #    exit()
+
                 X_start = int(X_centre - rect[1][0]/2 + X_MAX)
                 X_end = int(X_centre + rect[1][0]/2 + X_MAX)
                 Y_start = int(Y_centre - rect[1][1]/2 + Y_MAX)
                 Y_end = int(Y_centre + rect[1][1]/2 + Y_MAX)
+                crop_img = img_new[Y_start:Y_end, X_start:X_end]
+                if (crop_img.shape[1] < crop_img.shape[0]):
+                    crop_img = np.rot90(crop_img)
+                x_s = Threshold
+                if crop_img.shape[0]==0:
+                    continue
+                y_s = int((crop_img.shape[1]/crop_img.shape[0]) * Threshold)
+                crop_img = cv2.resize(crop_img,(y_s, x_s))
+                rest = MAX_LENGTH - crop_img.shape[1]
+                if rest < 0:
+                    rest = 0
+                stack = np.ones([Threshold, rest]) * 255
+                crop_img = np.hstack((crop_img, stack))
+
                 # if (X_start < 0):
                 #     M = np.float32([[1,0,-X_start],[0,1,0]])
                 #     img_new = cv2.warpAffine(img_new,M,(Y_MAX, X_MAX))
@@ -98,33 +126,25 @@ for filename in os.listdir('image_1000/'): #'/home/h/a/hanlins/Desktop/OCR/image
                 #     img_new = cv2.warpAffine(img_new,M,(Y_MAX, X_MAX))
                 #     Y_start = Y_start +Y_MAX - Y_end
                 #     Y_end = Y_MAX
-
-                # if (filename_suf == "TB1oAXWLXXXXXXoXXXXunYpLFXX" and Num==7):
-                #     print(X_start, X_end, Y_start, Y_end)
-                #     crop_img = img_new[Y_start:Y_end,X_start:X_end]
-                #     plt.imshow(img_new)
-                #     plt.show()
-                    #
-                    # cv2.drawContours(img,[box],-1,(0,255,0),3)
-                    # cv2.imshow("Image",img)
-                    # cv2.waitKey(0)
-                    #
-                    # exit()
-                # if (filename_suf=="TB1..FLLXXXXXbCXpXXunYpLFXX" and Num == 13):
-                #     print(X_start, X_end, Y_start, Y_end)
-                #     crop_img = img_new[Y_start:Y_end,X_start:X_end]
-                #     plt.imshow(crop_img)
-                #     plt.show()
+                # crop_img1 = crop_img
+                # if (crop_img.shape[0] > crop_img.shape[1]):
+                #     crop_img = crop_img.T
+                # if (crop_img1.shape[0] > Threshold):
+                #     crop_img1 = cv2.resize(crop_img1, (Threshold, int(crop_img1.shape[1]*(Threshold/crop_img1.shape[0]))))
+                # if (crop_img1.shape[1] > Threshold):
+                #     crop_img1 = cv2.resize(crop_img1, (int(crop_img1.shape[0]*(Threshold/crop_img1.shape[1])),Threshold))
+                # if crop_img1.shape[0] > MAX:
+                #     MAX = crop_img1.shape[0]
+                # if crop_img1.shape[1] > MAX:
+                #     MAX = crop_img1.shape[1]
+                # if(MAX == 2164):
+                #     print(crop_img1.shape)
+                #     print(crop_img.shape)
                 #     exit()
-
-                crop_img = img_new[Y_start:Y_end, X_start:X_end]
-                plt.imshow(crop_img, cmap="gray")
-                print(label, Num, '1')
+                # print(MAX)
                 rewrite_name = "rewrite/" + filename_suf + "_" + str(Num) + ".jpg" #"/home/h/a/hanlins/Desktop/OCR/rewrite/" + filename_suf + "_" + str(Num) + ".jpg"
-
+                cv2.imwrite(rewrite_name,crop_img)
                 ff.write(rewrite_name + ',' + label)
-
-                plt.savefig(rewrite_name)
                 Num += 1
 
 ff.close()
