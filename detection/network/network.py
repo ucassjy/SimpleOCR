72e7184d87f011d2d3d6b01a94ad5550a44a6fbe
ucassjy <470323747@qq.com>
2018-05-18 15:25:43 +0200

    FEAT : Can read the pretrained network

--- detection/network/network.py
+++ detection/network/network.py
@@ -1,12 +1,12 @@
-from vgg16 import vgg16
+from network import vgg16
 import tensorflow as tf
 
-class Network(Object):
+class Network(object):
     def __init__(self):
-        self.build_model()
-        self.init_saver()
+        x = 1
+        #self.init_saver()
 
-    def build_model(self, mode):
+    def build_network(self, mode, sess):
         self.is_training = mode == 'TRAIN'
 
         self._img = tf.placeholder(tf.float32, shape=[1, None, None, 3])
@@ -14,17 +14,20 @@ class Network(Object):
         self._gt_boxes = tf.placeholder(tf.float32, shape=[None, 5])
 
         # network architecture
-        d1 = tf.layers.dense(self.x, 512, activation=tf.nn.relu, name="dense1")
-        d2 = tf.layers.dense(d1, 10, name="dense2")
+        net = vgg16.img2fm(self._img)
+        net = vgg16.get_pretrained_net(sess, net)
+        print(net)
+        #d1 = tf.layers.dense(self.x, 512, activation=tf.nn.relu, name="dense1")
+        #d2 = tf.layers.dense(d1, 10, name="dense2")
 
-        with tf.name_scope("loss"):
-            self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y, logits=d2))
-            self.train_step = tf.train.AdamOptimizer(self.config.learning_rate).minimize(self.cross_entropy,
-                                                                                         global_step=self.global_step_tensor)
-            correct_prediction = tf.equal(tf.argmax(d2, 1), tf.argmax(self.y, 1))
-            self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        #with tf.name_scope("loss"):
+        #    self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y, logits=d2))
+        #    self.train_step = tf.train.AdamOptimizer(self.config.learning_rate).minimize(self.cross_entropy,
+        #                                                                                 global_step=self.global_step_tensor)
+        #    correct_prediction = tf.equal(tf.argmax(d2, 1), tf.argmax(self.y, 1))
+        #    self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 
-    def init_saver(self):
+#    def init_saver(self):
         # here you initialize the tensorflow saver that will be used in saving the checkpoints.
-        self.saver = tf.train.Saver(max_to_keep=self.config.max_to_keep)
+#        self.saver = tf.train.Saver()
