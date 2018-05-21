418f84b0f2f3bddf6c75c56e0ad0bffd694b7cad
ucassjy <470323747@qq.com>
2018-05-21 23:36:38 +0200

    FEAT: Change some code I don't know what they are for but I think it's right for R-anchors

--- detection/model/bbox.py
+++ detection/model/bbox.py
@@ -6,44 +6,45 @@ def bbox_transform(ex_rois, gt_rois):
     ex_heights = ex_rois[:, 2]
     ex_ctr_x = ex_rois[:, 0]
     ex_ctr_y = ex_rois[:, 1]
+    ex_angle = ex_rois[:, 4]
 
     gt_widths = gt_rois[:, 3]
     gt_heights = gt_rois[:, 2]
     gt_ctr_x = gt_rois[:, 0]
     gt_ctr_y = gt_rois[:, 1]
+    gt_angle = gt_rois[:, 4]
 
-    targets_dx = (gt_ctr_x - ex_ctr_x) / ex_widths
-    targets_dy = (gt_ctr_y - ex_ctr_y) / ex_heights
+    targets_dx = (gt_ctr_x - ex_ctr_x) / ex_widths / np.abs(np.sin(ex_rois[:, 4]))
+    targets_dy = (gt_ctr_y - ex_ctr_y) / ex_heights / np.abs(np.cos(ex_rois[:, 4]))
     targets_dw = np.log(gt_widths / ex_widths)
     targets_dh = np.log(gt_heights / ex_heights)
+    targets_dth = np.log(gt_angle / ex_angle)
 
     targets = np.vstack(
-        (targets_dx, targets_dy, targets_dw, targets_dh)).transpose()
+        (targets_dx, targets_dy, targets_dw, targets_dh, targets_dth)).transpose()
     return targets
 
 def bbox_transform_inv_tf(boxes, deltas):
     boxes = tf.cast(boxes, deltas.dtype)
-    widths = tf.subtract(boxes[:, 2], boxes[:, 0]) + 1.0
-    heights = tf.subtract(boxes[:, 3], boxes[:, 1]) + 1.0
-    ctr_x = tf.add(boxes[:, 0], widths * 0.5)
-    ctr_y = tf.add(boxes[:, 1], heights * 0.5)
+    widths = boxes[:, 3]
+    heights = boxes[:, 2]
+    ctr_x = boxes[:, 0]
+    ctr_y = boxes[:, 1]
+    angle = boxes[:, 4]
 
     dx = deltas[:, 0]
     dy = deltas[:, 1]
-    dw = deltas[:, 2]
-    dh = deltas[:, 3]
+    dh = deltas[:, 2]
+    dw = deltas[:, 3]
+    dth = deltas[:, 4]
 
     pred_ctr_x = tf.add(tf.multiply(dx, widths), ctr_x)
     pred_ctr_y = tf.add(tf.multiply(dy, heights), ctr_y)
     pred_w = tf.multiply(tf.exp(dw), widths)
     pred_h = tf.multiply(tf.exp(dh), heights)
+    pred_th = tf.multiply(tf.exp(dth), angle)
 
-    pred_boxes0 = tf.subtract(pred_ctr_x, pred_w * 0.5)
-    pred_boxes1 = tf.subtract(pred_ctr_y, pred_h * 0.5)
-    pred_boxes2 = tf.add(pred_ctr_x, pred_w * 0.5)
-    pred_boxes3 = tf.add(pred_ctr_y, pred_h * 0.5)
-
-    return tf.stack([pred_boxes0, pred_boxes1, pred_boxes2, pred_boxes3], axis=1)
+    return tf.stack([pred_ctr_x, pred_ctr_y, pred_h, pred_w, pred_th], axis=1)
 
 def clip_boxes_tf(boxes, im_info):
     x_center = boxes[:, 0]
