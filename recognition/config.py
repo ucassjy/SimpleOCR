0ee84bb5fbb45782533a66f92523093954abe071
Eric Sun <sunhanlin15@icloud.com>
2018-05-24 12:11:00 +0200

    Delete config.py

--- recognition/config.py
+++
@@ -1,7 +0,0 @@
-# Constants
-
-# Supported characters
-CHAR_VECTOR = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-'.!?,\""
-
-# Number of classes
-NUM_CLASSES = len(CHAR_VECTOR) + 1
